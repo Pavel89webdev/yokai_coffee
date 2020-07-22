aa44691f600b66254f18d06773bc7c9847dfe010
Pavel <erpavel1989@gmail.com>
2020-07-22 18:19:28 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+import tabs from './modules/tabs.js';
+import modal from './modules/modal.js';
+import timer from './modules/timer.js';
+import cards from './modules/cards.js';
+import calc from './modules/calc.js';
+import forms from './modules/forms.js';
+import slider from './modules/slider.js';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    const modalTimerId = setTimeout( () => openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2020-08-01');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        prevArrow: '.offer__slider-prev',
+        nextArrow: '.offer__slider-next',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+});
